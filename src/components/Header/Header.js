bcd08ac34d315a724b493ce6d555209f6a213c8f
yuhueilee <wendylee70127@gmail.com>
2024-03-12 11:16:34 +0800

    Add test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Header.css";
 
 export default function Header({ title }) {
-    return <h1 className="header">{title}</h1>;
+    return (
+        <h1 className="header" data-testid="header-1">
+            {title}
+        </h1>
+    );
 }
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -12,3 +12,9 @@ it("getByRole: should render same text passed into title prop", () => {
     const headingElement = screen.getByRole("heading", { name: "My Header" });
     expect(headingElement).toBeInTheDocument();
 });
+
+it("getByTestId: should render same text passed into title prop", () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByTestId("header-1");
+    expect(headingElement).toBeInTheDocument();
+});
