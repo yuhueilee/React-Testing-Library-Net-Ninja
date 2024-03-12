d9505fccf54c785052b1f0616105d7f264a00454
yuhueilee <wendylee70127@gmail.com>
2024-03-12 11:17:47 +0800

    Group into one test suite

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,20 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
-it("getByText: should render same text passed into title prop", () => {
-    render(<Header title="My Header" />);
-    const headingElement = screen.getByText(/my header/i);
-    expect(headingElement).toBeInTheDocument();
-});
+describe("getBy test cases: should render header component correctly", () => {
+    it("getByText: should render same text passed into title prop", () => {
+        render(<Header title="My Header" />);
+        const headingElement = screen.getByText(/my header/i);
+        expect(headingElement).toBeInTheDocument();
+    });
 
-it("getByRole: should render same text passed into title prop", () => {
-    render(<Header title="My Header" />);
-    const headingElement = screen.getByRole("heading", { name: "My Header" });
-    expect(headingElement).toBeInTheDocument();
-});
+    it("getByRole: should render same text passed into title prop", () => {
+        render(<Header title="My Header" />);
+        const headingElement = screen.getByRole("heading", {
+            name: "My Header",
+        });
+        expect(headingElement).toBeInTheDocument();
+    });
 
-it("getByTestId: should render same text passed into title prop", () => {
-    render(<Header title="My Header" />);
-    const headingElement = screen.getByTestId("header-1");
-    expect(headingElement).toBeInTheDocument();
+    it("getByTestId: should render same text passed into title prop", () => {
+        render(<Header title="My Header" />);
+        const headingElement = screen.getByTestId("header-1");
+        expect(headingElement).toBeInTheDocument();
+    });
 });
